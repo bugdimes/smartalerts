e08f7084dc24e5940f74d7a46fb98b0c75a44c7d
Divyesh Patel <pateldivyesh009@gmail.com>
2023-03-04 17:46:30 -0800

    endpoint and params transform

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -1,51 +1,31 @@
 class AlertsController < ApplicationController
-  before_action :set_alert, only: %i[ show update destroy ]
-
-  # GET /alerts
-  def index
-    @alerts = Alert.all
-
-    render json: @alerts
-  end
-
-  # GET /alerts/1
-  def show
-    render json: @alert
-  end
-
-  # POST /alerts
   def create
-    @alert = Alert.new(alert_params)
+    @alert = Alert.new(transform_params(alert_params))
 
     if @alert.save
-      render json: @alert, status: :created, location: @alert
+      render json: @alert, status: :created
     else
       render json: @alert.errors, status: :unprocessable_entity
     end
-  end
-
-  # PATCH/PUT /alerts/1
-  def update
-    if @alert.update(alert_params)
-      render json: @alert
-    else
-      render json: @alert.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /alerts/1
-  def destroy
-    @alert.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_alert
-      @alert = Alert.find(params[:id])
+    def alert_params
+      params.permit(:RecordType, :MessageStream, :Type, :TypeCode, :Name, :Tag, :Description, :Email, :From, :BouncedAt)
     end
 
-    # Only allow a list of trusted parameters through.
-    def alert_params
-      params.fetch(:alert, {})
+    def transform_params(params)
+      {
+        record_type: params[:RecordType],
+        message_stream: params[:MessageStream],
+        alert_type: params[:Type],
+        type_code: params[:TypeCode],
+        name: params[:Name],
+        tag: params[:Tag],
+        description: params[:Description],
+        email: params[:Email],
+        from: params[:From],
+        bounced_at: params[:BouncedAt]
+      }
     end
 end
